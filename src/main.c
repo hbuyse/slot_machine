550e0bd9f90cc893d3c410cdafd0a804bce22989
Henri Buyse <henri.buyse@eolane.com>
2016-05-02 15:15:42 +0200

    Add Doxygen mainpage

--- src/main.c
+++ src/main.c
@@ -23,6 +23,29 @@
 
 
 /**
+ * @mainpage Slot Machine Simulator - Documentation
+ *
+ * @section objectives Project's objectives
+ * This project consists in programming a slot machine simulator in C language.
+ *
+ *
+ * @section rules Rules of the game
+ * At the beginning of the game, the player has to deposit a certain number of credits (example: 100 credits).
+ * After each play, the player choose to bet 1, 2 or 3 credits.
+ * Then, the machine prints the gain in function of the combination which has appeared.
+ * Of course, the obtained gain crrespond to the base gain of the combination multiply by the number of credits betted
+ * by the player.
+ * The game continues while the player wishes it and that his credits are positive.
+ * The game automatically ends when the player has no more credits.
+ * The player can also decide to collect his credits at every moment of the game.
+ *
+ *
+ * @section author Author
+ * hbuyse (https://github.com/hbuyse)
+ */
+
+
+/**
  * @brief      Main program
  *
  * @return     Error code for the OS
